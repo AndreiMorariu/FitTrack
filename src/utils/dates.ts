ace5a05369ee268f4d390e21dae9268ebb2f8a24
AndreiMorariu <109082840+AndreiMorariu@users.noreply.github.com>
2024-11-02 21:34:02 +0200

    fixed format function

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -2,15 +2,15 @@ type DateFormat = "PPP" | "yyyy-MM-dd";
 
 export function formatDate(date: Date, format: DateFormat): string {
 	const year = date.getFullYear();
-	const month = date.getMonth();
-	const day = date.getDay();
+	const month = date.getMonth() + 1;
+	const day = date.getDate();
 
-	if (format === "yyyy-MM-dd") {
+	if (format === "PPP") {
 		const longMonth = date.toLocaleString("default", { month: "long" });
-		return `${longMonth}-${day}, ${year}`;
+		return `${longMonth} ${day}, ${year}`;
 	}
 
-	if (format === "PPP") return `${year}-${month}-${day}`;
+	if (format === "yyyy-MM-dd") return `${year}-${month}-${day}`;
 
 	throw new Error("Unsupported format");
 }
